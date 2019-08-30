3cd737881c23855b225dc9d55fc6cb0d9662f0b8
Andy Boughton <abought@gmail.com>
2019-08-30 16:52:29 -0400

    Fix default region for sample file

--- app.py
+++ app.py
@@ -25,11 +25,13 @@ def home():
 
 @app.route('/variant/<chrom_pos>/')
 def variant_view(chrom_pos):
+    # TODO: Allow query params to be passed from the base page to the api endpoint, so user can direct link to a
+    #   custom view
     chrom, pos = helpers.parse_position(chrom_pos)
     return render_template('phewas.html', chrom=chrom, pos=pos)
 
 
-@app.route('/api/variant/<chrom_pos>/')
+@app.route('/api/variant/<chrom_pos>/', methods=['GET'])
 def query(chrom_pos):
     """An API endpoint that returns data in nicely formatted JSON. Fetching data as JSON allows a single HTML file
     to update interactively without reloading (with appropriate supporting page code)."""
