7a69defcdeae15827bf660c4c7ead2a8573ebd0f
Alan Kwong <amkwong@umich.edu>
2019-09-19 16:20:56 -0400

    Added a utility script to symlink the necessary files when setting up with access to files on the UM StatGen cluster.

---
+++ util/create.UM.statgen.links.sh
@@ -0,0 +1,2 @@
+ln -s /net/amd/amkwong/browseQTL/all_chr/ensembl/Homo_sapiens.GRCh38.97.chr.gff3.gz ../data/
+ln -s /net/amd/amkwong/browseQTL/all_chr/data/by_chromosome/chr*.All_Tissues.sorted.txt.gz* ../data/
