1e97f1bd41588d5ea99bb16fb107c9fd047472c6
Alan Kwong <amkwong@umich.edu>
2019-09-30 16:48:59 -0400

    Added script for users within the UM StatGen cluster. Running this script creates links to pre-processed files in the data directory, avoiding the need to process the full GTEx v8 data.

---
+++ util/create.UM.statgen.links.sh
@@ -0,0 +1,4 @@
+ln -s /net/amd/amkwong/browseQTL/all_chr/ensembl/Homo_sapiens.GRCh38.97.chr.gff3.gz ../data/
+ln -s /net/amd/amkwong/browseQTL/all_chr/data/by_chromosome/chr*.All_Tissues.sorted.txt.gz* ../data/
+ln -s /net/amd/amkwong/browseQTL/all_chr/ensembl/gene.symbol.pickle ./
+ln -s /net/amd/amkwong/browseQTL/all_chr/ensembl/gene.chrom.pos.lookup.sqlite3.db ../data/
