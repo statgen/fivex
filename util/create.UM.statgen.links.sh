8cd48651e8fe8592b4fd8aa8138b4443d1cd2696
Alan Kwong <amkwong@umich.edu>
2019-10-02 18:32:31 -0400

    Fixed a typo

--- util/create.UM.statgen.links.sh
+++ util/create.UM.statgen.links.sh
@@ -2,4 +2,4 @@ ln -s /net/amd/amkwong/browseQTL/all_chr/ensembl/Homo_sapiens.GRCh38.97.chr.gff3
 ln -s /net/amd/amkwong/browseQTL/all_chr/data/by_chromosome/chr*.All_Tissues.sorted.txt.gz* ../data/
 ln -s /net/amd/amkwong/browseQTL/all_chr/ensembl/gene.symbol.pickle ./
 ln -s /net/amd/amkwong/browseQTL/all_chr/ensembl/gene.chrom.pos.lookup.sqlite3.db ../data/
-ln -s /net/amd/amkwong/browseQTL/all_chr/ensembl/ID.only.gff3.gz* ./
+ln -s /net/amd/amkwong/browseQTL/all_chr/ensembl/ID.only.gff3.gz* ../data/
