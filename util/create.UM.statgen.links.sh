6495ae114fc911b56c9f152ec10680bd64f7f9cf
Alan Kwong <amkwong@umich.edu>
2019-10-10 15:22:28 -0400

    Deleted redundant rm lines

--- util/create.UM.statgen.links.sh
+++ util/create.UM.statgen.links.sh
@@ -1,7 +1,3 @@
-rm ../data/chr*.All_Tissues.sorted.txt.gz*
-rm ../data/gene.symbol.pickle
-rm ../data/gene.chrom.pos.lookup.sqlite3.db
-rm ../data/ID.only.gff3.gz*
 ln -f -s -t ../data/ /net/amd/amkwong/browseQTL/all_chr/ensembl/Homo_sapiens.GRCh38.97.chr.gff3.gz
 ln -f -s -t ../data/ /net/amd/amkwong/browseQTL/all_chr/data/by_chromosome/chr*.All_Tissues.sorted.txt.gz*
 ln -f -s -t ../data/ /net/amd/amkwong/browseQTL/all_chr/ensembl/gene.symbol.pickle
