9e11ff7ac1019f2945bd531ee2089a50706c949d
Peter VandeHaar <pjvandehaar@gmail.com>
2019-10-29 14:44:36 -0400

    Use genelocator to show nearest genes to the page's variant

--- pheget/views/variant.py
+++ pheget/views/variant.py
@@ -3,9 +3,12 @@ Variant View Page.
 """
 
 from flask import render_template
+from genelocator import get_genelocator
 
 import pheget
 from pheget.views.format import parse_position
+
+gl = get_genelocator('GRCh38', gencode_version=32, coding_only=True)
 
 
 @pheget.app.route('/variant/<chrom_pos>/')
@@ -13,4 +16,5 @@ def variant_view(chrom_pos):
     # TODO: Allow query params to be passed from the base page to the api endpoint, so user can direct link to a
     #   custom view
     chrom, pos = parse_position(chrom_pos)
-    return render_template('phewas.html', chrom=chrom, pos=pos)
+    nearest_genes = gl.at(chrom, pos)
+    return render_template('phewas.html', chrom=chrom, pos=pos, nearest_genes=nearest_genes)
