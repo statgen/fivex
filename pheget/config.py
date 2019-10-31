dc0b1b78729d2246c3dfa52caed863a193942b3e
Alan Kwong <amkwong@umich.edu>
2019-10-31 11:13:39 -0400

    Cleaned up config.py and added note about how to set up localconfig.py

--- pheget/config.py
+++ pheget/config.py
@@ -7,8 +7,12 @@ import os
 # Root of this application, useful if it doesn't occupy an entire domain
 APPLICATION_ROOT = '/'
 
-#DATA_DIR = '/home/amkwong/backup' # 'data'
+# Default data directory - this contains both variant-level (tabixed) data files and supporting databases
 DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'data')
+
+# To set up a default local data directory, create "localconfig.py" in this directory and 
+# define DATA_DIR to be the static local path to that directory. This will override the default
+# DATA_DIR and make it easier to automate updates.
 
 # Database file is data/, currently not using
 DATABASE_FILENAME = os.path.join(
