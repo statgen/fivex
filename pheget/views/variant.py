e95009f5f2482164145d3ad7fd486c52c39af526
Andy Boughton <abought@gmail.com>
2019-11-08 09:23:39 -0500

    Simplify logic on gene display & handle error cases

--- pheget/views/variant.py
+++ pheget/views/variant.py
@@ -3,6 +3,7 @@ Variant View Page.
 """
 
 from flask import render_template
+from genelocator import exception as gene_exc
 from genelocator import get_genelocator
 
 import pheget
@@ -16,5 +17,16 @@ def variant_view(chrom_pos):
     # TODO: Allow query params to be passed from the base page to the api endpoint, so user can direct link to a
     #   custom view
     chrom, pos = parse_position(chrom_pos)
-    nearest_genes = gl.at(chrom, pos)
-    return render_template('phewas.html', chrom=chrom, pos=pos, nearest_genes=nearest_genes)
+
+    try:
+        nearest_genes = gl.at(chrom, pos)
+    except (gene_exc.NoResultsFoundException, gene_exc.BadCoordinateException):
+        nearest_genes = []
+
+    # Are the "nearest genes" nearby, or is the variant actually inside the gene?
+    # These rules are based on the defined behavior of the genes locator
+    is_inside_gene = (len(nearest_genes) > 1 or
+                      (len(nearest_genes) == 1 and nearest_genes[0]['start'] <= pos <= nearest_genes[0]['end']))
+
+    return render_template('phewas.html',
+                           chrom=chrom, pos=pos, nearest_genes=nearest_genes, is_inside_gene=is_inside_gene)
