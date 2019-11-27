886f0bb04250f0fb44f645ba856557c6e0d4a8b5
Alan Kwong <amkwong@umich.edu>
2019-11-27 16:08:45 -0500

    Widened default region view when searching for a gene from the searchbox, and added comments

--- pheget/static/js/searchbox.js
+++ pheget/static/js/searchbox.js
@@ -26,30 +26,33 @@ function parseSearch(searchText) {
         const pos = cMatch[3];
         window.location = `/variant/${chrom}_${pos}`;
     } else if (cMatchRange !== null && searchText === cMatchRange[0]) {
+        // If the query is a range in the form [chr]:[start]-[end], go to a region view page of that range
         const chrom = cMatchRange[2];
         const start = cMatchRange[3];
         const end = cMatchRange[4];
         window.location = `/region/?chrom=${chrom}&start=${start}&end=${end}`;
     } else {
-        // Use omnisearch - works especially well for rs numbers
+        // Use omnisearch to parse any other query - works especially well for rs numbers and gene names
         const omniurl = 'https://portaldev.sph.umich.edu/api/v1/annotation/omnisearch/?q=' + searchText + '&build=GRCh38';
         getOmni(omniurl, function (err, omniJson) {
             if (err !== null) {
                 alert('Error encountered: ' + err);
             } else {
+                // If the result is a single variant, parse the position and try to go to the single variant view
+                // Note: this works because omnisearch will return start=end even for indels
                 if (omniJson.data[0].chrom !== undefined && omniJson.data[0].start === omniJson.data[0].end) {
                     const chrom = omniJson.data[0].chrom;
                     const pos = omniJson.data[0].start;
                     window.location = `/variant/${chrom}_${pos}`;
                 } else if (omniJson.data[0].gene_id !== undefined) {
+                    // If the result is a gene, get the range of the gene, pad it on both sides, and go to a region view
                     const chrom = omniJson.data[0].chrom;
                     const gene_id = omniJson.data[0].gene_id;
-                    const start = omniJson.data[0].start;
-                    const end = omniJson.data[0].end;
+                    const start = Math.max(omniJson.data[0].start - 300000, 1);
+                    const end = omniJson.data[0].end + 300000;
                     window.location = `/region/?chrom=${chrom}&gene_id=${gene_id}&start=${start}&end=${end}`;
-                    // alert('You searched for the following gene: ' + omniJson.data[0].gene_name + '.\nSorry, we currently do not support region view (Will be added later).');
                 } else {
-                    alert('Sorry, we were unable to find variant "' + searchText + '" in our database.');
+                    alert('Sorry, we were unable to find a match for the search term "' + searchText + '" in our database.');
                 }
             }
         });
