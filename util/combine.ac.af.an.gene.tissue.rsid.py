1683af27eca25f57010d8c2a57cb0105b53db84c
Alan Kwong <amkwong@umich.edu>
2019-12-13 01:16:11 -0500

    Finished fixing script to combine AC/AF/AN, best tissue/gene, and rsid

--- util/combine.ac.af.an.gene.tissue.rsid.py
+++ util/combine.ac.af.an.gene.tissue.rsid.py
@@ -36,8 +36,7 @@ def bestGen(fileObj):
 
 
 def rangeGen():
-    # chrList = list(range(1,23)) + ['X']
-    chrList = ["19", "20"]
+    chrList = [str(i) for i in range(1, 23)] + ["X"]
     idx = 0
     while True:
         yield (f"chr{chrList[idx]}")
@@ -52,19 +51,19 @@ def main():
         "infile",
         metavar="INFILE",
         type=str,
-        help="Input with with best tissues and genes",
+        help="Input file with with AC/AF/AN",
     )
     parser.add_argument(
         "prefix",
         metavar="PREFIX",
         type=str,
-        help="Input file prefix with AC/AF/AN (before chr#)",
+        help="Input file prefix with best tissues and genes(before chr#)",
     )
     parser.add_argument(
         "suffix",
         metavar="SUFFIX",
         type=str,
-        help="Input file suffix with AC/AF/AN (after chr#)",
+        help="Input file suffix with best tissues and genes (after chr#)",
     )
     parser.add_argument(
         "outfile",
@@ -80,40 +79,26 @@ def main():
     )
 
     args = parser.parse_args()
+
+    # f will iterate through a file containing all AC/AF/AN (about 46 million variants) in one file
+    # s contains all rs numbers retrieved from the lookup table file, found on GTExPortal
     with gzip.open(args.infile) as f, open(
         args.outfile, "w"
     ) as w, shelve.open(args.shelve) as s:
         bgzip = subprocess.Popen(
             ["bgzip", "-c"], stdin=subprocess.PIPE, stdout=w
         )
-        genoGen = gtexGen(f)
-        (chrom, pos, ref, alt, ac, af, an) = next(genoGen)  # Initialize
-        chrRangeGen = rangeGen()
+        genoGen = gtexGen(f)  # ACAFAN
+        (chrom, pos, ref, alt, ac, af, an) = next(genoGen)  # Initialize values
+        chrRangeGen = rangeGen()  # Initialize the chromosome generator
         currentChr = next(chrRangeGen)
-        alleleInfile = f"{args.prefix}{currentChr}{args.suffix}"
-        g = gzip.open(alleleInfile)
-        alleleInfoGen = bestGen(g)
-        (gchr, gpos, gref, galt, gene, tissue) = next(alleleInfoGen)
+        bestInfile = f"{args.prefix}{currentChr}{args.suffix}"  # This contains best gene/tissue information
+        g = gzip.open(bestInfile)
+        bestInfoGen = bestGen(g)
+        (gchr, gpos, gref, galt, gene, tissue) = next(
+            bestInfoGen
+        )  # Initialize values
         while True:
-            # If they are different chromosomes, close the current 'top' file and open the next one if possible, break otherwise
-            if gchr != chrom:
-                g.close()
-                try:
-                    currentChr = next(chrRangeGen)
-                    infile = f"{args.prefix}{currentChr}{args.suffix}"
-                    g = open(infile)
-                    alleleInfoGen = bestGen(g)
-                except StopIteration:
-                    break
-            # If chromosome is the same but allele info is ahead of the genotype generator, advance the genotype generator
-            while gchr == chrom and gpos > pos:
-                (chrom, pos, ref, alt, ac, af, an) = next(genoGen)
-            # If the genotype generator skipped the current variant, then something seriously went wrong and we need to stop
-            if gpos < pos:
-                print(
-                    "Error: information for {chrom}:{pos}:{ref}:{alt} not found, exiting"
-                )
-                sys.exit(2)
             # If all fields match, find the rsid and write to the output: chrom pos ref alt gene tissue AC AF AN rsid
             # Then advance both generators and continue
             if gchr == chrom and gpos == pos and gref == ref and galt == alt:
@@ -124,10 +109,44 @@ def main():
                     )
                 )
                 try:
-                    (chrom, pos, ref, alt, ac, af, an) = next(genoGen)
+                    (chrom, pos, ref, alt, ac, af, an) = next(
+                        genoGen
+                    )  # One file, will not run out
+                except StopIteration:
+                    break
+                try:
                     (gchr, gpos, gref, galt, gene, tissue) = next(
-                        alleleInfoGen
-                    )
+                        bestInfoGen
+                    )  # Multiple files, will run out
+                except StopIteration:
+                    g.close()
+                    try:
+                        currentChr = next(chrRangeGen)
+                        bestInfile = f"{args.prefix}{currentChr}{args.suffix}"
+                        g = gzip.open(bestInfile)
+                        bestInfoGen = bestGen(g)
+                        (gchr, gpos, gref, galt, gene, tissue) = next(
+                            bestInfoGen
+                        )
+                        continue
+                    except IndexError:
+                        break
+            # If they are different chromosomes, close the current 'top' file and open the next one if possible, break otherwise
+            # If chromosome is the same but allele info is ahead of the genotype generator, advance the genotype generator
+            if gchr == chrom and gpos > pos:
+                try:
+                    (chrom, pos, ref, alt, ac, af, an) = next(genoGen)
+                    continue
+                except StopIteration:
+                    break
+            # If the genotype generator skipped the current variant, then something seriously went wrong and we need to stop
+            if gchr == chrom and gpos < pos:
+                sys.exit(2)
+            # if chromosomes are different, advance the best AC/AF/AN file
+            if gchr != chrom:
+                try:
+                    (chrom, pos, ref, alt, ac, af, an) = next(genoGen)
+                    continue
                 except StopIteration:
                     break
         bgzip.communicate()
