5aea3dfe4ee65ce20afacad2f483f7a67db1f737
Andy Boughton <abought@gmail.com>
2020-03-03 11:47:49 -0500

    Re-hydrate "extra" gene and tissue tracks based on URL on first page load

--- src/views/Region.vue
+++ src/views/Region.vue
@@ -16,6 +16,9 @@ const MAX_EXTENT = 1000000;
  * @param {Object} queryParams
  */
 function getData(queryParams) {
+  // FIXME: Our Vue page has some special options (like y_field) that don't matter to the API
+  //  but currently get passed anyway. That's mildly silly and will break if Vue and the API use
+  //  the same parameter to mean different things.
   const params = $.param(queryParams);
   return fetch(`/backend/views/region/?${params}`)
     .then(handleErrors)
@@ -490,7 +493,7 @@ export default {
       start: null,
       end: null,
 
-      // Internal state (UI and buttons)
+      // Fields whose values affect options and/or URL query params
       y_field: 'log_pvalue',
       gene_id: null,
       tissue: null,
@@ -498,15 +501,18 @@ export default {
       extra_genes: [],
       extra_tissues: [],
 
+      // Internal state
       loading_done: false,
     };
   },
   computed: {
     short_gene_id() {
+      // Strip versions from ENSG identifiers for display (ENSG0.1 -> ENSG0)
       const { gene_id } = this;
       return gene_id && gene_id.split('.')[0];
     },
     query_params() {
+      // Re-calculate the URL query string whenever dependent information changes.
       const {
         chrom, start, end, gene_id, tissue, y_field, extra_genes, extra_tissues,
       } = this;
@@ -522,11 +528,11 @@ export default {
       if (extra_tissues.length) {
         options.extra_tissues = extra_tissues;
       }
-
       return $.param(options);
     },
   },
   beforeCreate() {
+    // See: https://router.vuejs.org/guide/advanced/data-fetching.html#fetching-before-navigation
     // Preserve a reference to component widgets so that their methods can be accessed directly
     //  Some- esp LZ plots- behave very oddly when wrapped as a nested observable; we can
     //  bypass these problems by assigning them as static properties instead of nested
@@ -534,14 +540,15 @@ export default {
     this.assoc_plot = null;
     this.assoc_sources = null;
   },
-  // See: https://router.vuejs.org/guide/advanced/data-fetching.html#fetching-before-navigation
   beforeRouteEnter(to, from, next) {
     // First navigation to route
     // TODO: Catch navigation failures (eg bad api call, no data, etc)
-    // FIXME: Use query params, and proxy whatever given to route, then rewrite url at end
     getData(to.query)
       .then((data) => {
-        next((vm) => vm.setData(data));
+        next((vm) => {
+          vm.setQuery(to.query);
+          vm.setData(data);
+        });
       });
   },
   beforeRouteUpdate(to, from, next) {
@@ -551,6 +558,7 @@ export default {
     this.assoc_sources = null;
 
     getData(to.query).then((data) => {
+      this.setQuery(to.query);
       this.setData(data);
       next();
     });
@@ -558,7 +566,8 @@ export default {
   updated() {
     // Popper tooltips depend on dynamic data. They must be initialized after the component
     //   has finished rendering.
-    // TODO: This would reinitialize tooltips every time a radio button is selected, which seems slightly wasteful
+    // TODO: This would reinitialize tooltips every time a radio button is selected, which seems slightly wasteful.
+    //   Vue bootstrap addon has some nicer features to massage the differences between Vue and BS
     this.$nextTick(() => {
       $('[data-toggle="tooltip"]').tooltip();
       $('[data-toggle-second="tooltip"]').tooltip();
@@ -574,14 +583,23 @@ export default {
         },
       });
     },
+    setQuery(params) {
+      // Set some default query params
+      this.y_field = params.y_field || this.y_field;
+      // Our url serializer (`$.param`) serializes array params as `key[]`; convert to `key` format
+      const { 'extra_genes[]': extra_genes, 'extra_tissues[]': extra_tissues } = params;
+      if (extra_genes) {
+        this.extra_genes = Array.isArray(extra_genes) ? extra_genes : [extra_genes];
+      }
+      if (extra_tissues) {
+        this.extra_tissues = Array.isArray(extra_tissues) ? extra_tissues : [extra_tissues];
+      }
+    },
     setData(data) {
       // Convert passed params to instance variables. Also create plot and do other reactive things.
       this.region_data = data;
 
       if (data) {
-        // TODO: Improve how we set default query param options
-        this.y_field = this.$route.query.y_field || this.query;
-
         const {
           chrom: chr, start, end, gene_id, tissue, symbol,
         } = data;
@@ -595,12 +613,34 @@ export default {
         this.gene_id = gene_id;
         this.tissue = tissue;
 
-        const track_panels = getTrackLayout(gene_id, tissue, initialState, symbol);
-        // TODO: Make base plot layout respond to y_field query param
+        // TODO: Refactor to break plot creation out separate from data parsing
+        // Create track layouts for the basic (anchor) track, plus any extra ones to be added to the plot
+        const gene_tracks = this.extra_genes.slice()
+          .map((item) => getTrackLayout(item, tissue, initialState, data.gene_list[item]))
+          .flat();
+        const tissue_tracks = this.extra_tissues.slice()
+          .map((item) => getTrackLayout(gene_id, item, initialState, symbol))
+          .flat();
+        const track_panels = [
+          ...getTrackLayout(gene_id, tissue, initialState, symbol),
+          ...gene_tracks,
+          ...tissue_tracks,
+        ];
         this.base_plot_layout = getBasicLayout(initialState, track_panels);
-        const track_sources = getTrackSources(gene_id, tissue);
 
-        // TODO: Should we add track sources for the other "optional" tracks, when creating the base plot layout?
+        // Create data sources for the basic (anchor) track, plus any extra ones to be added to the plot
+        const gene_sources = this.extra_genes.slice()
+          .map((item) => getTrackSources(item, tissue))
+          .flat();
+        const tissue_sources = this.extra_tissues.slice()
+          .map((item) => getTrackSources(gene_id, item))
+          .flat();
+        const track_sources = [
+          ...getTrackSources(gene_id, tissue), // Plot the anchor gene and tissue first
+          ...gene_sources, // ...then any extra tracks relative to those anchors
+          ...tissue_sources,
+        ];
+
         this.base_plot_sources = getBasicSources(track_sources);
       }
       this.loading_done = !!data;
@@ -625,18 +665,19 @@ export default {
     addTrack(type, track_id) {
       if (type === 'gene') {
         const { gene_list } = this.region_data;
-        const { extra_gene } = this;
         let extra_gene_symbol;
-        if (!extra_gene || !gene_list) {
+        if (!track_id || !gene_list) {
           extra_gene_symbol = null;
         } else {
-          extra_gene_symbol = gene_list[extra_gene];
+          extra_gene_symbol = gene_list[track_id];
         }
         addTrack(this.assoc_plot, this.assoc_sources, track_id, this.tissue, extra_gene_symbol);
+        this.extra_genes.push(track_id);
       }
 
       if (type === 'tissue') {
         addTrack(this.assoc_plot, this.assoc_sources, this.gene_id, track_id, this.region_data.symbol);
+        this.extra_tissues.push(track_id);
       }
     },
   },
@@ -648,11 +689,20 @@ export default {
       this.$nextTick(() => switchY_region(this.assoc_plot, this.y_field));
     },
     query_params() {
-      // Update the URL whenever query params change (including at first page load)
+      // Update the URL whenever anything would change the query params
+      //  (including at first page load, if the server, eg, fills in best gene/tissue for region)
+
       // We're very intentionally bypassing the Vue router functions here. Those would trigger
       //   a full page reload whenever params change, but we want to do incremental things
       //   (like adding plot panels) that would not benefit from a reload.
-      window.history.pushState({}, document.title, `?${this.query_params}`);
+
+      // TODO: For now, we replaceState (eg, clicking the back button does not skip to the exact last region/ set of panels)
+      //   instead of history.pushState.
+      // This is because vue router assumes that the URL defines the total state of the application,
+      //    and tries to forcibly reload the whole page when URL changes (eg back button). This is
+      //    more re-rendering than we want. We will prioritize bookmarking the current view,
+      //    at the expense of the back button.
+      window.history.replaceState({}, document.title, `?${this.query_params}`);
     },
   },
   components: {
