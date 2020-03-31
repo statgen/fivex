5fc5f2dc34139e7abe491e951347fd37d926f12c
Andy Boughton <abought@gmail.com>
2020-03-31 17:32:16 -0400

    Fix issue where API redirects were breaking, but only in local dev mode
    Hackety hack!
    Context: `https://github.com/chimurai/http-proxy-middleware/issues/140#issuecomment-275270924`

--- vue.config.js
+++ vue.config.js
@@ -17,6 +17,19 @@ module.exports = {
         target: 'http://localhost:5000',
         ws: false,
         changeOrigin: true,
+        onProxyRes(proxyRes, req, res) {
+          // When flask redirects internally, it includes its own host and port, and we get CORS
+          //  errors as suddenly the proxied request is going to a different "server".
+          //  We fix that by rewriting the location header manually.
+          // TODO: This is an ugly hack related to limitations of webpack dev server; see:
+          //  https://github.com/chimurai/http-proxy-middleware/issues/140#issuecomment-275270924
+          //  (note this rewrite is crude and will break locally if flask is run on a port other than 5000)
+          if ([301, 302].includes(proxyRes.statusCode) && proxyRes.headers.location) {
+            let redirect = proxyRes.headers.location;
+            redirect = redirect.replace('http://localhost:5000', '/api');
+            proxyRes.headers.location = redirect;
+          }
+        },
         pathRewrite: { '^/api': '' },
       },
     },
