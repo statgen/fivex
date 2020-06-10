e953f7d989ec2c175bfd1d091abf1eb1bddef03b
amkwong <amkwong@umich.edu>
2020-06-10 00:16:03 -0400

    Added links to Open Target Genetics and GWAS Catalog

--- src/views/Variant.vue
+++ src/views/Variant.vue
@@ -496,6 +496,16 @@ export default {
                    aria-pressed="true" v-b-tooltip.top
                    title="Summary of 3,144 GWAS of Brain Imaging Derived Phenotypes (IDPs) in 9,707 participants from the UK Biobank, analyzed with the BGENIE software">
                   UKB-Oxford BIG <span class="fa fa-external-link-alt"></span></a>
+                <a :href="`https://genetics.opentargets.org/variant/${ chrom }_${ pos }_${ ref }_${ alt }`"
+                   target="_blank" class="btn btn-secondary btn-sm mr-1" role="button"
+                   aria-pressed="true" v-b-tooltop.top
+                   title="Open Target Genetics, a public-private partnership between Bristol Myers Squibb, GlaxoSmithKlein, Sanofi, and EMBL-EBI to allow browsing of genes, variants, and traits">
+                  Open Targets Genetics <span class="fa fa-external-link-alt"></span></a>
+                <a :href="`https://www.ebi.ac.uk/gwas/search?query=${ rsid }`"
+                   target="_blank" class="btn btn-secondary btn-sm mr-1" role="button"
+                   aria-pressed="true" v-b-tooltop.top
+                   title="The NHGRI-EBI Catalog of published genome-wide association studies, providing an updated and professionally curated database of published GWAS.">
+                  GWAS Catalog <span class="fa fa-external-link-alt"></span></a>
               </template>
             </div>
           </div>
