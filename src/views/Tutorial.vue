12931001aaedbe881d14fd0ecab87c34c1d37d6d
amkwong <amkwong@umich.edu>
2020-10-29 17:17:29 -0400

    Addressed spacing and casing warnings

--- src/views/Tutorial.vue
+++ src/views/Tutorial.vue
@@ -4,33 +4,45 @@
       <div class="col-sm-12">
         <h1>Tutorial</h1>
         <p>
-          <video controls src="../../media/FIVEx_tutorial_1.mp4" type="video/mp4">
-           Your browser does not support the HTML5 Video element. Here is a <a href="media/FIVEx_tutorial_1.mp4">link</a> instead.
+          <video controls
+           src="../../media/FIVEx_tutorial_1.mp4"
+           type="video/mp4">
+            Your browser does not support the HTML5 Video element.
           </video>
         </p>
         <p>
-          <video controls src="../../media/FIVEx_tutorial_2.mp4" type="video/mp4">
-           Your browser does not support the HTML5 Video element.
+          <video controls
+           src="../../media/FIVEx_tutorial_2.mp4"
+           type="video/mp4">
+            Your browser does not support the HTML5 Video element.
           </video>
         </p>
         <p>
-          <video controls src="../../media/FIVEx_tutorial_3.mp4" type="video/mp4">
-           Your browser does not support the HTML5 Video element.
+          <video controls
+           src="../../media/FIVEx_tutorial_3.mp4"
+           type="video/mp4">
+            Your browser does not support the HTML5 Video element.
           </video>
         </p>
         <p>
-          <video controls src="../../media/FIVEx_tutorial_4.mp4" type="video/mp4">
-           Your browser does not support the HTML5 Video element.
+          <video controls
+           src="../../media/FIVEx_tutorial_4.mp4"
+           type="video/mp4">
+            Your browser does not support the HTML5 Video element.
           </video>
         </p>
         <p>
-          <video controls src="../../media/FIVEx_tutorial_5.mp4" type="video/mp4">
-           Your browser does not support the HTML5 Video element.
+          <video controls
+           src="../../media/FIVEx_tutorial_5.mp4"
+           type="video/mp4">
+            Your browser does not support the HTML5 Video element.
           </video>
         </p>
         <p>
-          <video controls src="../../media/FIVEx_tutorial_6.mp4" type="video/mp4">
-           Your browser does not support the HTML5 Video element.
+          <video controls
+           src="../../media/FIVEx_tutorial_6.mp4"
+           type="video/mp4">
+            Your browser does not support the HTML5 Video element.
           </video>
         </p>
         <p><small>&copy; {{ (new Date()).getFullYear() }} - The University of Michigan, Center for Statistical Genetics</small></p>
@@ -41,7 +53,7 @@
 
 <script>
 export default {
-    name: 'tutorial',
+    name: 'Tutorial',
 };
 </script>
 
