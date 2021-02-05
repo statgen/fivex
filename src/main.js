3b239eed2d8eb305363a5c383c3cffd128115d88
Andy Boughton <abought@gmail.com>
2021-02-05 12:44:11 -0500

    Add Google analytics tracking

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue';
+import VueGtag from 'vue-gtag';
+
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+if (process.env.VUE_APP_GOOGLE_ANALYTICS) {
+    // Track page and router views in google analytics
+    Vue.use(VueGtag, {
+        config: { id: process.env.VUE_APP_GOOGLE_ANALYTICS },
+        appName: 'FIVEx',
+        pageTrackerScreenviewEnabled: true,
+    }, router);
+}
+
 
 new Vue({
     router,
