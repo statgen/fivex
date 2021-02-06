827f62d93d8061edc246e4c9855097fe087262df
amkwong <amkwong@umich.edu>
2021-02-06 14:18:43 -0500

    Updated instructions on finding data files for UM internal devs

--- util/create.UM.statgen.links.sh
+++ util/create.UM.statgen.links.sh
@@ -1,6 +1,1 @@
-ln -f -s -t ../data/ /net/amd/amkwong/browseQTL/all_chr/ensembl/Homo_sapiens.GRCh38.97.chr.gff3.gz
-ln -f -s -t ../data/ /net/amd/amkwong/browseQTL/all_chr/data/by_chromosome/chr*.All_Tissues.sorted.txt.gz*
-ln -f -s -t ../data/ /net/amd/amkwong/browseQTL/all_chr/ensembl/gene.symbol.pickle
-ln -f -s -t ../data/ /net/amd/amkwong/browseQTL/all_chr/ensembl/gene.chrom.pos.lookup.sqlite3.db
-ln -f -s -t ../data/ /net/amd/amkwong/browseQTL/all_chr/ensembl/ID.only.gff3.gz*
-ln -f -s -t ../data/ /net/amd/amkwong/browseQTL/all_chr/GTEx_v8_finemapping_DAPG.sqlite.db
+ln -f -s -t ../data /net/amd/amkwong/FIVEx/data/*
