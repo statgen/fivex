497009b631a7d4a686e718690b7ca07841ec5f07
amkwong <amkwong@umich.edu>
2021-04-19 17:39:31 -0400

    Modified the help text a bit

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -5,25 +5,25 @@
         <h1>Help</h1>
         <h2>Understanding Posterior Inclusion Probabilities and DAP-G</h2>
         <p>
-          A common problem in interpreting eQTL results is to untangle the evolutionary relationships between variants that are frequently inherited together, in order to understand which (and how many) variants are directly influencing observed biology.
+          A longstanding issue in using eQTL association results to elucidate biological mechanisms has been the difficulty of identifying true causal  variants for gene expression in the presence of linkage disequilibrium (LD). This is further complicated by the use of P-values in evaluating eQTL significance, which may fail to convey uncertainty about the association results.
         </p>
 
         <p>
-          To address this question, FIVEx incorporates results from DAP-G, a fine-mapping procedure designed to highlight variants of particular significance. [<a href="https://journals.plos.org/plosgenetics/article?id=10.1371/journal.pgen.1006646">1</a>][<a href="https://www.biorxiv.org/content/10.1101/316471v1">2</a>][<a href="https://storage.googleapis.com/gtex_analysis_v8/single_tissue_qtl_data/GTEx_v8_finemapping_DAPG.tar">3</a>] DAP-G provides two useful pieces of information:
+          To address these issues, FIVEx incorporates results from DAP-G, a Bayesian fine-mapping method designed to highlight significantly associated variants and quantify the uncertainty of those associations. [<a href="https://journals.plos.org/plosgenetics/article?id=10.1371/journal.pgen.1006646">1</a>][<a href="https://www.biorxiv.org/content/10.1101/316471v1">2</a>][<a href="https://storage.googleapis.com/gtex_analysis_v8/single_tissue_qtl_data/GTEx_v8_finemapping_DAPG.tar">3</a>] DAP-G provides two useful pieces of information:
         </p>
 
         <ol>
-          <li>Calculated Posterior Inclusion Probability (PIP): how likely is it that this variant influences the expression of a given gene in a given tissue?</li>
-          <li>Clustering of significance results: how many different (groups of) variants will independently influence this expression?</li>
+          <li>Posterior Inclusion Probability (PIP): how likely is it that this variant influences the expression of a given gene in a given tissue?</li>
+          <li>Clustering of significance signals: how many different groups of variants independently influence this expression?</li>
         </ol>
 
         <p>
-          In the <em> region view</em>, each cluster will represent an independent signal (eg, variants that are in high LD with each other), and clusters are ranked with numbers based on how strongly they influence the eQTL: cluster 1 has the strongest influence, then cluster 2, etc) The sum of PIP values (SPIP) can be used to identify if this cluster represents a signal of particular interest. Typically there are not many independent signals for a given eQTL, and as a result, FIVEx only shows the top 4 clusters.
+          In the <em>region view</em>, variants which appear to be associated with changes in gene expression are grouped into clusters based on LD. Each cluster represents an independent signal. These clusters are ranked based on the strength of the model's belief in the eQTL: cluster 1 has the strongest signal, followed by cluster 2, and so on). The signal-level PIP (SPIP), equal to the sum of all PIPs within a cluster, indicates the total strength of the signal for that cluster. To make these results easier to understand at a glance, FIVEx only provides unique symbols for the top 4 clusters.
         </p>
 
-      <p>
-        In the single-variant view, results are summarized across many different genes or tissues. In this case, the DAP-G PIPs and cluster information are not directly comparable, but they provide a quick visual summary: "show me any genes and tissues where this variant was of particular interest".
-      </p>
+        <p>
+          In the single-variant view, eQTL associations are summarized across many different genes and tissues, making it easier to see how much a variant may affect the expression of a gene across multiple tissues, or the expression of multiple genes in the same tissue. 
+        </p>
 
         <h2>Glossary</h2>
         FIVEx brings together data from many different sources. The GTEx project provides
@@ -34,17 +34,13 @@
           <dt>Normalized Effect Size (NES)</dt>
           <dd>
             The normalized effect size (NES) of the eQTLs is computed as the effect of the alternative allele (ALT)
-            relative to the reference allele (REF).
-          </dd>
-
-          <dt>PIP, SPIP, and cluster</dt>
-          <dd>
-            See the help text above for more details.
+            relative to the reference allele (REF), divided by the sample standard deviation to yield a dimensionless magnitude.
+            Please note that due to the way NES is calculated, it has no direct biological interpretation.
           </dd>
 
           <dt>Transcription Start Site (TSS) distance</dt>
           <dd>
-            The distance (in base pairs, bp) from a given variant to the transcription start site of the specified gene. Often (though not always), variants close to a gene are more likely to be impactful on expression levels of that gene.
+            The distance (in base pairs, bp) from a given variant to the transcription start site of the specified gene. Often (though not always), variants which signficiantly affect expression levels of a gene are found near the TSS.
           </dd>
         </dl>
       </div>
