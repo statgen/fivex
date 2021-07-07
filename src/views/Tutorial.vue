f0c6fa2f20205e7c2d294d37ff4ccbe4184f03cf
amkwong <amkwong@umich.edu>
2021-07-07 03:30:52 -0700

    Added descriptions to tutorial videos (descriptions are intended for updated videos)

--- src/views/Tutorial.vue
+++ src/views/Tutorial.vue
@@ -3,7 +3,8 @@
     <div class="row">
       <div class="col-sm-12">
         <h1>Tutorial</h1>
-        <p>
+        <h2>Navigating FIVEx's single variant view</h2>
+        <p>          
           <video
             controls
             src="../../media/FIVEx_tutorial_1.mp4"
@@ -12,6 +13,7 @@
             Your browser does not support the HTML5 Video element.
           </video>
         </p>
+        <h2>Using the buttons on the single-variant page</h2>
         <p>
           <video
             controls
@@ -21,6 +23,7 @@
             Your browser does not support the HTML5 Video element.
           </video>
         </p>
+        <h2>Using filters with user-chosen thresholds and the table in single variant view</h2>
         <p>
           <video
             controls
@@ -30,6 +33,7 @@
             Your browser does not support the HTML5 Video element.
           </video>
         </p>
+        <h2>Switching between eQTLs and sQTLs</h2>
         <p>
           <video
             controls
@@ -39,6 +43,7 @@
             Your browser does not support the HTML5 Video element.
           </video>
         </p>
+        <h2>FIVEx's region view</h2>
         <p>
           <video
             controls
@@ -48,6 +53,7 @@
             Your browser does not support the HTML5 Video element.
           </video>
         </p>
+        <h2>Region view table, and moving between single variant and region views</h2>
         <p>
           <video
             controls
